f9e8d2bf40a5d4df5b1a73c16a712084f6ceaad9
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-13 19:33:30 -0400

    post mock data

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -9,14 +9,29 @@ function Feed() {
     <div className="feed">
       <StoryReel />
       <MessageSender />
-      <Post />
-      <Post />
+
       <Post
         profilePic="https://lh3.googleusercontent.com/proxy/JliIjDjp3jQ62TYWe1vCMud_s854Wgw38A7h6ajpL8qYHlKmvV-Bv8vnGYaE2tQtxmReHwDtig8ooFxOBQrQ-ZaGy_YSMoFOglzl0ngcWYvs"
-        message="wow"
-        timestamp="timestamp"
+        message="wow delicious suchi :) Must try it out!"
+        timestamp="Jan 21"
         username="tiff0000"
         image="https://lh3.googleusercontent.com/proxy/vIr0CHHaUn9bJLdDcI3eVJvABAKXohMHXg2EhD-9D5aXoXhS2zHZaGP-6nACk4X0oZq4545GRMX6CEGURcXnuiRl2zZJr_FrghPhyc-Bu_j3ojvw09ALCLNgtw"
+      />
+
+      <Post
+        profilePic="https://www.toux8.com/uploads/allimg/180302/04145T934-0.jpg"
+        message="wow"
+        timestamp="Jan 4"
+        username="Bill"
+        image="https://www.sbs.com.au/popasia/sites/sbs.com.au.popasia/files/1millionjaypark.png"
+      />
+
+      <Post
+        profilePic="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSS5XwsHcKvELNEiA--SJSCK-eZeo6oUO4JKA&usqp=CAU"
+        message="Today's workout is so hard! Just got home. Feeling exhausted. "
+        timestamp="Jan 1"
+        username="Lucy"
+        image=""
       />
       {/* MessageSender */}
     </div>
--- fb-clone/src/Post.js
+++ fb-clone/src/Post.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+
+import NearMeIcon from "@material-ui/icons/NearMe";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Post({ profilePic, image, username, timestamp, message }) {
   return (
@@ -9,7 +15,7 @@ function Post({ profilePic, image, username, timestamp, message }) {
         <Avatar src={profilePic} className="post__avatar" />
         <div className="post__topInfo">
           <h3>{username}</h3>
-          <p>TimeStamp..</p>
+          <p>{timestamp}</p>
           {/* <p>{new Date(timestamp?.toDate()).toUTCString}</p> */}
         </div>
       </div>
@@ -20,6 +26,28 @@ function Post({ profilePic, image, username, timestamp, message }) {
 
       <div className="post__image">
         <img src={image} alt="" />
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreIcon />
+        </div>
       </div>
     </div>
   );
