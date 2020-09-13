3cba86522b531ebe440c736fa535053b0223532d
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-13 12:46:14 -0400

    starter files, menu bar with styling done

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIeTabynEK8ZeE_JSlVYK5EqEhRe7sMjs",
+  authDomain: "fb-clone-50a64.firebaseapp.com",
+  databaseURL: "https://fb-clone-50a64.firebaseio.com",
+  projectId: "fb-clone-50a64",
+  storageBucket: "fb-clone-50a64.appspot.com",
+  messagingSenderId: "418829394521",
+  appId: "1:418829394521:web:8009c20eb6fba23190c748",
+  measurementId: "G-7NDG0RRLGN",
+};
