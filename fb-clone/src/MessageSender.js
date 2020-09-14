eadac2b21a5a2e27c04756eefed63c479197617a
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-13 23:03:07 -0400

    debugging feed news

--- fb-clone/src/MessageSender.js
+++ fb-clone/src/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from "@material-ui/icons/Videocam";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import { useStateValue } from "./StateProvider";
+import db from "./firebase";
+import firebase from "./firebase";
 
 function MessageSender() {
   const [input, setInput] = useState("");
@@ -13,6 +15,16 @@ function MessageSender() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // add to db when user enter new post
+    db.collection("posts").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp().now(),
+      profilePic: user.photoURL,
+      username: user.displayName,
+      image: imageUrl,
+    });
+
     setInput("");
     setImageUrl("");
   };
